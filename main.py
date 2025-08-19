ca0b55de950d6020a0db42081de830eff5b2b033
Vedang <vedangvishalrauL@gmail.com>
2025-08-19 17:11:01 +0530

    Series integrated

--- main.py
+++ main.py
@@ -95,3 +95,23 @@ async def show_pop_movies():
             raise HTTPException(status_code=exc.response.status_code,detail=f"Error connecting to the TMDB API: {exc.response.text}")
         except httpx.RequestError as exc:
             raise HTTPException(status_code=503,detail=f"Error connecting to the TMDB API server: {exc}")
+@app.get("/api/search/tv/{query}")
+async def search_series(query:str):
+    if not TMDB_API_KEY:
+        raise HTTPException(status_code=500,detail="TMDB API key is not configured.")
+    search_url=f"{TMDB_API_URL}/search/tv"
+    params={
+        "api_key": TMDB_API_KEY,
+        "query": query,
+        "language": "en-US"
+    }
+    async with httpx.AsyncClient() as client: #This line create a fresh request page with is sent to the API
+        try:
+            response = await client.get(search_url,params=params) #This is the main event, this is where we send the request to 
+                                                                  #the API for the movie. using search_url and params
+            response.raise_for_status()  # Raise an error for bad responses
+            return response.json()  #This line parses (coverts kinda) the response to JSON format and returns it
+        except httpx.HTTPStatusError as exc: #This block catches erros when you successfully connected to the API but the API rejected your request
+            raise HTTPException(status_code=exc.response.status_code,detail=f"Error from TMDB API{exc.response.text}") #Take the error msg sent from TMDB and show it to user
+        except httpx.RequestError as exc: #This block catches errors when you couldn't connect to the API at all
+            raise HTTPException(status_code=503,detail=f"Error connecting to TMDB API server{exc}")
