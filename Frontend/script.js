e8a60c00a0be1f15f9ae1b7048687bc19af576f1
Vedang <vedangvishalrauL@gmail.com>
2025-08-19 16:35:53 +0530

    Error message updated

--- Frontend/script.js
+++ Frontend/script.js
@@ -3,6 +3,30 @@ const searchBtn = document.getElementById('searchBtn');
 const movieInput = document.getElementById('movieInput');
 const resultsDiv = document.getElementById('results');
 const loader= document.getElementById('loader');
+const errorMessageDiv = document.getElementById('error-message');
+
+function showErr(message) {
+    errorMessageDiv.innerHTML = `
+        <div class="error">
+            <div class="error__icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" height="24" fill="none">
+                    <path fill="#393a37" d="m13 13h-2v-6h2zm0 4h-2v-2h2zm-1-15c-1.3132 0-2.61358.25866-3.82683.7612-1.21326.50255-2.31565 1.23915-3.24424 2.16773-1.87536 1.87537-2.92893 4.41891-2.92893 7.07107 0 2.6522 1.05357 5.1957 2.92893 7.0711.92859.9286 2.03098 1.6651 3.24424 2.1677 1.21325.5025 2.51363.7612 3.82683.7612 2.6522 0 5.1957-1.0536 7.0711-2.9289 1.8753-1.8754 2.9289-4.4189 2.9289-7.0711 0-1.3132-.2587-2.61358-.7612-3.82683-.5026-1.21326-1.2391-2.31565-2.1677-3.24424-.9286-.92858-2.031-1.66518-3.2443-2.16773-1.2132-.50254-2.5136-.7612-3.8268-.7612z"></path>
+                </svg>
+            </div>
+            <div class="error__title">${message}</div>
+            <div class="error__close">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" viewBox="0 0 20 20" height="20">
+                    <path fill="#393a37" d="m15.8333 5.34166-1.175-1.175-4.6583 4.65834-4.65833-4.65834-1.175 1.175 4.65833 4.65834-4.65833 4.6583 1.175 1.175 4.65833-4.6583 4.6583 4.6583 1.175-1.175-4.6583-4.6583z"></path>
+                </svg>
+            </div>
+        </div>
+    `;
+    // Add the event listener to the close button
+    const closeBtn = errorMessageDiv.querySelector('.error__close');
+    closeBtn.addEventListener('click', () => {
+        errorMessageDiv.innerHTML = '';
+    });
+}
 
 
 // --- 2. Add an event listener for the button click ---
@@ -22,7 +46,7 @@ async function searchMovies() {
 
     // Don't search if the input is empty
     if (movieTitle === "") {
-        resultsDiv.innerHTML = '<p class="error-message">Please enter a movie title.</p>';
+        showErr('Please enter a movie title to search.');
         return;
     }
     
@@ -48,7 +72,7 @@ async function searchMovies() {
     } catch (error) {
         console.error('Fetch error:', error);
         // Display a user-friendly error message on the page
-        resultsDiv.innerHTML = `<p class="error-message">Oops! ${error.message}</p>`;
+        showErr(`Oops !${error.message}`);    
     }
 }
 async function Pop_movies(){
@@ -70,7 +94,7 @@ async function Pop_movies(){
 }catch (error) {
         console.error('Fetch error:', error);
         // Display a user-friendly error message on the page
-        resultsDiv.innerHTML = `<p class="error-message">Oops! ${error.message}</p>`;
+        showErr(`Oops !${error.message}`);
     }
     finally {
         loader.style.display = 'none';
